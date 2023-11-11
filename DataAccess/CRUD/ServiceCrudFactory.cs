87b3bfca1f99ffa72dfb629f566aa0c65cce06ec
Jafet Calderon <jcalderonca@ucenfotec.ac.cr>
2023-11-11 13:41:57 -0600

    Working on Service Crud Factory

---
+++ DataAccess/CRUD/ServiceCrudFactory.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.DAOs;
+using DataAccess.Mapper;
+using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.CRUD
+{
+    public class ServiceCrudFactory : CrudFactory <Service>
+    {
+        private readonly UserMapper _mapper;
+        protected SqlDao _dao;
+        public ServiceCrudFactory()
+        {
+            _mapper = new UserMapper();
+            _dao = SqlDao.GetInstance();
+        }
+
+    }
+}

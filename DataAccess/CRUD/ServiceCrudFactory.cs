6a57365d7d0385e2f08438d4a7139f118e8210e8
Jafet Calderon <jcalderonca@ucenfotec.ac.cr>
2023-11-13 20:12:08 -0600

    Service Backend finished 11.13.2023

--- DataAccess/CRUD/ServiceCrudFactory.cs
+++ DataAccess/CRUD/ServiceCrudFactory.cs
@@ -11,13 +11,68 @@ namespace DataAccess.CRUD
 {
     public class ServiceCrudFactory : CrudFactory <Service>
     {
-        private readonly UserMapper _mapper;
+        private readonly ServiceMapper _mapper;
         protected SqlDao _dao;
         public ServiceCrudFactory()
         {
-            _mapper = new UserMapper();
+            _mapper = new ServiceMapper();
             _dao = SqlDao.GetInstance();
         }
+        public override void Create(Service dto)
+        {
+            var sqlOperation = new SqlOperation("CREATE_SERVICE_PR");
+            sqlOperation.AddParameter("@P_SERVICE_NAME", dto.ServiceName);
+            sqlOperation.AddParameter("@P_STATUS", dto.ServiceStatus);
+            sqlOperation.AddParameter("@P_DESCRIPTION", dto.ServiceDescription);
+            sqlOperation.AddParameter("@P_COST", dto.ServiceCost);
+            sqlOperation.AddParameter("@P_CREATED_DATE", dto.ServiceCreatedDate);
+            sqlOperation.AddParameter("@P_MODIFIED_DATE", dto.ServiceModifiedDate);
+            sqlOperation.AddParameter("@P_ID", dto.Id); // Preguntar porque estamos enviando el id y no se esta autogenerando en la base de datos.
+            _dao.ExecuteProcedure(sqlOperation);
+
+        }
+        public override void Update(Service dto)
+        {
+            var sqlOperation = new SqlOperation("UPDATE_SERVICE_PR");
+            sqlOperation.AddParameter("@P_SERVICE_ID", dto.Id);
+            sqlOperation.AddParameter("@P_SERVICE_NAME", dto.ServiceName);
+            sqlOperation.AddParameter("@P_STATUS", dto.ServiceStatus);
+            sqlOperation.AddParameter("@P_DESCRIPTION", dto.ServiceDescription);
+            sqlOperation.AddParameter("@P_COST", dto.ServiceCost);
+            sqlOperation.AddParameter("@P_MODIFIED_DATE", dto.ServiceModifiedDate);
+            _dao.ExecuteProcedure(sqlOperation);
+        }
+        public override void Delete(int id)
+        {
+            var sqlOperation = new SqlOperation("DELETE_SERVICE_PR");
+            sqlOperation.AddParameter("@P_SERVICE_ID", id);
+            _dao.ExecuteProcedure(sqlOperation);
+        }
+        public override Service RetrieveById(int id)
+        {
+            var sqlOperation = new SqlOperation("RETRIEVE_SERVICE_BY_ID_PR");
+            sqlOperation.AddParameter("@P_SERVICE_ID", id);
+            var lstResult = _dao.ExecuteQueryProcedure(sqlOperation);
+            if (lstResult.Count > 0)
+            {
+                var dto = _mapper.BuildObject(lstResult[0]);
+                return dto;
+            }
+            return null;
+        }
+        public override List<Service>? RetrieveAll()
+        {
+            var sqlOperation = new SqlOperation("RETRIEVE_ALL_SERVICES_PR");
+            var lstResult = _dao.ExecuteQueryProcedure(sqlOperation);
+            if (lstResult.Count > 0)
+            {
+                var lstServices = _mapper.BuildObjects(lstResult);
+                return lstServices;
+            }
+            return null;
+        }
+
 
     }
 }
+/*        @P_SERVICE_ID INT */ 
---
+++ DataAccess/Mapper/ServiceMapper.cs
@@ -0,0 +1,40 @@
+﻿using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Mapper
+{
+    public class ServiceMapper : Mapper<Service>
+    {
+        public override Service BuildObject(Dictionary<string, object> row)
+        {
+            var service = new Service
+            {
+                Id = (int)row["service_id"],
+                ServiceName = (string)row["service_name"],
+                ServiceDescription = (string)row["service_description"],
+                ServiceStatus = (int)row["service_status"],
+                ServiceCost = (decimal)row["service_cost"],
+                ServiceCreatedDate = (DateTime)row["service_created_date"],
+                ServiceModifiedDate = (DateTime)row["service_modified_date"]
+            };
+            return service;
+        }
+
+        public List<Service> BuildObjects(List<Dictionary<string, object>> lstRows)
+        {
+            var lstResults = new List<Service>();
+
+            foreach (var item in lstRows)
+            {
+                var service = BuildObject(item);
+                lstResults.Add(service);
+            }
+
+            return lstResults;
+        }   
+    }
+}

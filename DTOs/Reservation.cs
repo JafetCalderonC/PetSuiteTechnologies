9285ffdf0c8b27c582d65361c4880c21e6719c86
Jafet Calderon <jcalderonca@ucenfotec.ac.cr>
2023-12-02 20:52:23 -0600

    Pendiente listar perros y paquetes en reservaciones

---
+++ DTOs/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class Reservation : BaseDTO
+    {
+        public int ReservationID { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+
+        public int UserID { get; set; }
+        public int? PetId { get; set; }
+        public int PackageId { get; set; }
+        public DateTime? ReservationCreatedDate { get; set; }
+        public DateTime? ReservationModifiedDate { get; set; }
+    }
+}
